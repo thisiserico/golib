776f86ca099bac8b68c4de55af72af6cc4e51d06
Eric Lopez <hello@ericlopez.me>
2019-09-06 03:30:38 +0200

    provide constant package with keys and values

---
+++ constant/constant_test.go
@@ -0,0 +1,19 @@
+package constant
+
+import "testing"
+
+func Suite(t *testing.T, k Key, v Value, expected interface{}) {
+	if value := v.Value(); value != expected {
+		t.Fatalf("%s -> want %v, got %v", k, expected, value)
+	}
+}
+
+func TestConstants(t *testing.T) {
+	Suite(t, BuildID, OneBuildID("build ID"), "build ID")
+	Suite(t, CorrelationID, OneCorrelationID("correlation ID"), "correlation ID")
+	Suite(t, InBehalfOf, InBehalfOfServiceName("service name"), "service name")
+	Suite(t, IsDryRun, IsDryRunExecution(true), true)
+	Suite(t, ServiceHost, RunningInHost("service host"), "service host")
+	Suite(t, ServiceName, RunningService("service name"), "service name")
+	Suite(t, WhosRequesting, RequestedByServiceName("service name"), "service name")
+}
