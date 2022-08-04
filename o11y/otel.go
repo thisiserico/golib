6e569529fe01869b1921e33760ac40fb1adde0b1
Eric Lopez <hello@ericlopez.me>
2022-08-04 10:56:17 +0200

    wip amend

--- o11y/otel.go
+++ o11y/otel.go
@@ -7,15 +7,17 @@ import (
 
 	"github.com/thisiserico/golib/kv"
 	"github.com/thisiserico/golib/logger"
+	"github.com/thisiserico/golib/oops"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/sdk/trace"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // Configure sets up opentelemetry to use golib functionality.
 // Specifically, an otel tracer that uses the logger is set.
 func Configure(log logger.Log) {
-	tracer := trace.NewTracerProvider(trace.WithSpanProcessor(newSpanProcessor(log)))
+	tracer := tracesdk.NewTracerProvider(tracesdk.WithSpanProcessor(newSpanProcessor(log)))
 	otel.SetTracerProvider(tracer)
 }
 
@@ -26,4 +28,28 @@ func Attributes(ctx context.Context) []attribute.KeyValue {
 		attribute.String("correlation_id", kv.CorrelationID(ctx).String()),
 		attribute.Bool("is_dry_run", kv.IsDryRun(ctx).Bool()),
 	}
+}
+
+func RecordError(span trace.Span, err error) {
+	var attributes []attribute.KeyValue
+	for _, detail := range oops.Details(err) {
+		var attr attribute.KeyValue
+		name := detail.Name()
+
+		switch value := detail.Value(); value.(type) {
+		case string:
+			attr = attribute.String(name, value.(string))
+
+		case int:
+			attr = attribute.Int(name, value.(int))
+
+		case bool:
+			attr = attribute.Bool(name, value.(bool))
+		}
+
+		attributes = append(attributes, attr)
+	}
+
+	span.SetAttributes(attributes...)
+	span.RecordError(err)
 }
